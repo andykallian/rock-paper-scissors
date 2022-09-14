a18a0f8d04a97c99badd3636605186b364155161
Anderson Viana <andersonkallian@yahoo.com.br>
2022-09-14 01:22:10 -0300

    adding JS funcionalities

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 blueElement = document.querySelector(".blue");
 yellowElement = document.querySelector(".yellow");
 redeElement = document.querySelector(".red");
+number = document.querySelector('.number');
+result = document.querySelector('.result')
 
 selection = document.querySelectorAll(".img-container");
+reset = document.querySelector('.reset');
+
+playAgain = document.querySelector('.play-again');
 
 let options = [1,2,3]
+let score = 0;
 let machineRandon;
 
 let rock;
@@ -14,17 +20,34 @@ let paper;
 let user;
 let machine;
 
+number.innerHTML = 0;
+
+function updateScore(){
+  if(user > machine){
+    score++;
+    result.innerHTML = 'YOU WIN';
+  }else if(user < machine){
+    result.innerHTML = 'YOU LOSE';
+    score--;
+  }
+  number.innerHTML = score;
+}
+
 
 selection.forEach((element, index) => {  
-    selection[index].addEventListener("click", function(){
+    selection[index].addEventListener("click", function choice(){
       if(element.classList.contains('red')){
         user = 'rock';
+        element.classList.add('user-choice')
         machineRandon = Math.floor(Math.random()*options.length+1)
+        document.querySelector('.images-container').classList.add('row-reverse')
       }else if(element.classList.contains('yellow')){
         user = 'scissors';
+        element.classList.add('user-choice')
         machineRandon = Math.floor(Math.random()*options.length+1)
       }else if(element.classList.contains('blue')){
         user = 'paper'
+        element.classList.add('user-choice')
         machineRandon = Math.floor(Math.random()*options.length+1)
       }
   
@@ -35,8 +58,8 @@ selection.forEach((element, index) => {
             paper = 3;
             scissors = 1;
             user = rock;
-            blueElement.style.display = "none";
-            yellowElement.style.display = "none";
+            blueElement.classList.add('hidden');
+            yellowElement.classList.add('hidden');
             redeElement.style.boxShadow = "2px 0px 42px 15px rgba(0,0,0,0.42)"
             yellowElement.style.boxShadow = "none"
             blueElement.style.boxShadow = "none"
@@ -47,8 +70,8 @@ selection.forEach((element, index) => {
             paper = 2;
             scissors = 3;
             user = paper;
-            redeElement.style.display = "none";
-            yellowElement.style.display = "none";
+            redeElement.classList.add('hidden');
+            yellowElement.classList.add('hidden');
             blueElement.style.boxShadow = "2px 0px 42px 15px rgba(0,0,0,0.42)"
             redeElement.style.boxShadow = "none"
             yellowElement.style.boxShadow = "none"
@@ -59,8 +82,8 @@ selection.forEach((element, index) => {
             paper = 1;
             scissors = 2;
             user = scissors;
-            redeElement.style.display = "none";
-            blueElement.style.display = "none";
+            redeElement.classList.add('hidden');
+            blueElement.classList.add('hidden');
             yellowElement.style.boxShadow = "2px 0px 42px 15px rgba(0,0,0,0.42)"
             redeElement.style.boxShadow = "none"
             blueElement.style.boxShadow = "none"
@@ -92,61 +115,40 @@ selection.forEach((element, index) => {
    
       if(machine === rock){
         redeElement.style.display = "flex";
+        redeElement.classList.add('machine-choice')
+        redeElement.classList.remove('hidden');
+        document.querySelector('.images-container').classList.add("align-container")
       }else if(machine === paper){
         blueElement.style.display = "flex";
+        blueElement.classList.add('machine-choice')
+        blueElement.classList.remove('hidden');
+        document.querySelector('.images-container').classList.add('align-container')
       }else if(machine === scissors){
         yellowElement.style.display = "flex";
+        yellowElement.classList.add('machine-choice')
+        yellowElement.classList.remove('hidden');
+        document.querySelector('.images-container').classList.add('align-container')
       }
+
+      playAgain.classList.remove('hide')
+      blueElement.classList.add('not-clickable')
+      redeElement.classList.add('not-clickable')
+      yellowElement.classList.add('not-clickable')
+
+      updateScore(score)
 
     })
 });
 
 
+reset.addEventListener("click", () =>{
 
+  let classes = ['user-choice', 'machine-choice', 'hidden', 'not-clickable']
 
+  blueElement.classList.remove(...classes);
+  redeElement.classList.remove(...classes);
+  yellowElement.classList.remove(...classes);
 
+  playAgain.classList.add('hide')
 
-
-
-
-
-
-// let options = [1,2,3,4,5,6];
-
-
-// let machineOption = Math.floor(Math.random()*options.length+1);
-// let machine;
-
-
-
-// switch(machineOption){
-//   case 1:
-//   case 2:{
-//     machine = pedra
-//     break
-//   }
-//   case 3:
-//   case 4:{
-//     machine = papel
-//     break
-//   }
-//   case 5: 
-//   case 6:{
-//     machine = tesoura
-//     break
-//   }
-// }
-
-// console.log(`A escolha do usuário foi: ${usuario}`)
-// console.log(`A escolha da maquina foi: ${machine}`)
-
-// if(usuario > machine){
-//   console.log('usuario ganhou')
-// }else if(machine > usuario){
-//   console.log('a maquina ganhou')
-// }else{
-//   console.log('empate')
-// }
-
-
-
+})
